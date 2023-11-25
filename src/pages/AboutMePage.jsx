177fd7d8a518dd7ccbd8dd615a9ea9b64b3f6b37
AJ Seidler <ajseidler0526@gmail.com>
2023-11-25 11:39:47 -0600

    updated about me page

--- src/pages/AboutMePage.jsx
+++ src/pages/AboutMePage.jsx
@@ -1,8 +1,9 @@
 // This is a static page mocking an "About Us" section for our fake user data
 export default function AboutMePage() {
   return (
-    <div className="container pt-4">
-      <p>
+    <div className="p-4 d-flex pt-4">
+      <img src="src/assets/aj-siedler-936x1024.webp" alt="" className="abt-me-port"/>
+      <p className="ps-4">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
         tenetur maiores, dolor iusto dolorum ullam, natus deleniti blanditiis
         impedit suscipit sed magnam alias in, repellat expedita hic explicabo
