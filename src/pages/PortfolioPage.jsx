23ff3c68c2e02adfd2b0739ed26ebf4a3f7dd7c7
AJ Seidler <ajseidler0526@gmail.com>
2023-11-27 22:36:51 -0600

    fixed pathing

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -1,12 +1,12 @@
 import ProjectCard from '../components/UI/ProjectCard'
-import socialTaskApp from '../assets/task-img.png'
+import socialTaskApp from '../assets/tasks-img.png'
 
 const projects = [
   {
     name: 'Full-Stack Social Task App',
     url: 'https://peaceful-beach-14774-2857744df451.herokuapp.com/',
     img: socialTaskApp,
-    github: 'https://github.com/MrDietCola/Social-Task-App',
+    github: 'https://github.com/Seidler93/Social-Task-App',
   },
   {
     name: 'Event and Hotel Finder',
@@ -18,25 +18,25 @@ const projects = [
     name: 'NoSQL Employee Tracker',
     url: 'https://youtu.be/jyVmS1fM8W0',
     img: 'src/assets/Screenshot (25).png',
-    github: 'https://github.com/MrDietCola/Employee-tracker',
+    github: 'https://github.com/Seidler93/Employee-tracker',
   },
   {
     name: 'PWA Text Editor',
     url: 'https://limitless-inlet-49962-fdf6a772dad4.herokuapp.com/',
     img: 'src/assets/Screenshot (32).png',
-    github: 'https://github.com/MrDietCola/PWA-Text-Editor',
+    github: 'https://github.com/Seidler93/PWA-Text-Editor',
   },
   {
     name: 'Full-Stack Note Taker',
     url: 'https://gentle-coast-49570-f78783b9d58f.herokuapp.com/notes',
     img: 'src/assets/Screenshot (20).png',
-    github: 'https://github.com/MrDietCola/Note-taker-2.0',
+    github: 'https://github.com/Seidler93/Note-taker-2.0',
   },
   {
     name: 'MONGODB Ecommerce',
     url: 'https://www.youtube.com/watch?v=7q9dMxxWjxY&ab_channel=AJSeidler',
     img: 'src/assets/Screenshot (27).png',
-    github: 'https://github.com/MrDietCola/Ecommerce',
+    github: 'https://github.com/Seidler93/Ecommerce',
   },
 ]
 
